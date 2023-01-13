d881de919a76d4e6708dfc471c30f4f7cc9759f2
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2023-01-13 08:35:45 +0100

    Updates index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,11 @@ import Head from "next/head";
 import Image from "next/image";
 import classNames from "classnames/bind";
 
-
 import sumImg from "../public/images/sumImg.svg";
 
 import styles from "../styles/Home.module.css";
 
 const c = classNames.bind(styles);
-const poppins = Poppins({ weight: "500" });
 
 export default function Home() {
   return (
@@ -19,12 +17,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.header}>
-          <h1 className={clsx(poppins.className, styles.heading)}>Prosum</h1>
-          <p className={clsx(barlow.className, styles.subHeading)}>
-            Share exciting project summaries
-          </p>
+      <main className={c("main")}>
+        <div className={c("header")}>
+          <h1 className={c("heading")}>Prosum</h1>
+          <p className={c("subHeading")}>Share exciting project summaries</p>
         </div>
 
         <div className={styles.image}>
@@ -32,8 +28,8 @@ export default function Home() {
         </div>
 
         <div className={styles.projectLoader}>
-          <h3 className={poppins.className}>Project status</h3>
-          <p className={clsx(barlow.className, styles.projectPercent)}>10%</p>
+          <h3>Project status</h3>
+          <p className={c("projectPercent")}>10%</p>
         </div>
       </main>
     </>
