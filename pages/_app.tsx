b49c0ee518d4cde095cfc480dea0a4619a2c39e1
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2023-01-13 08:30:44 +0100

    Updates document fonts to Poppins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Poppins } from "@next/font/google";
+
+import "../styles/globals.css";
+
+const poppins = Poppins({ weight: ["400", "700"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
